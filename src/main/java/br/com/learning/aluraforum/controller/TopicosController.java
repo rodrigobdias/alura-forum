98e4fc8d50f36596048d849bc02629faf5473ef1
Rodrigo <rodrigobdias@gmail.com>
2019-08-16 15:07:07 -0300

    Removendo tópicos

--- src/main/java/br/com/learning/aluraforum/controller/TopicosController.java
+++ src/main/java/br/com/learning/aluraforum/controller/TopicosController.java
@@ -44,6 +44,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		
 		Topico topico = form.converter(cursoRepository);
@@ -69,6 +70,14 @@ public class TopicosController {
 		Topico topico = form.atualizar(id, topicoRepository);
 
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id){
+		topicoRepository.deleteById(id);
+
+		return  ResponseEntity.ok().build();
 	}
 
 }
